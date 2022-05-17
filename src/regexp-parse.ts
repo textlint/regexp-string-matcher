71f02c9cceeabe812c92a98ae3473dc24dda8156
azu <azu@users.noreply.github.com>
2022-05-18 08:27:16 +0900

    feat: support s and d flag (#12)
    - `s` means dotAll
    - `d` flag is just ignored

--- src/regexp-parse.ts
+++ src/regexp-parse.ts
@@ -1,4 +1,5 @@
-const REGEXP_LITERAL_PATTERN = /^\/(.*)\/([guimy]*)$/;
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#advanced_searching_with_flags
+const REGEXP_LITERAL_PATTERN = /^\/(.*)\/([guimysd]*)$/;
 export const parseRegExpString = (str: string): { source: string; flagString: string } | null => {
     const result = str.match(REGEXP_LITERAL_PATTERN);
     if (!result) {
---
+++ test/snapshots/dotAll-regexp/output-for-human.md
@@ -0,0 +1,16 @@
+
+- match text: **1
+2
+3
+**
+- captures: **[]**
+- startIndex: **0**
+- endIndex: **6**
+
+```
+**1
+2
+3
+**
+```
+
